fc4ae2a8cf7caaf7d9c544bb174deeafe16b9dea
snowdensb <snowdesb@yahoo.com>
2025-04-08 15:33:20 -0400

    Update ParentConfig.java

--- src/main/java/org/owasp/webgoat/server/ParentConfig.java
+++ src/main/java/org/owasp/webgoat/server/ParentConfig.java
@@ -2,6 +2,8 @@ package org.owasp.webgoat.server;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ConfigurationClassUtils;
+
 
 @Configuration
 @ComponentScan("org.owasp.webgoat.server")
